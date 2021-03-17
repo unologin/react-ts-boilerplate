32d74c6d368b287892996df6d9cf327ec87b1797
adrian <adrian@unolog.in>
2021-03-17 14:59:40 +0100

    typescript webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,58 @@
+
+import webpack from 'webpack';
+import path from 'path';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import dotenv from 'dotenv';
+import ESLintPlugin from 'eslint-webpack-plugin';
+
+const env = dotenv.config({path: __dirname + '/.env'});
+
+const config =
+{
+  entry: './src/main.tsx',
+  output:
+  {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: 
+  {
+    rules: 
+    [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 
+        [
+          'babel-loader',
+          'ts-loader',
+        ],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: 
+  [
+    new HTMLWebpackPlugin(
+      {
+        template: 'public/index.html',
+        filename: 'index.html',
+      }
+    ),
+    new ESLintPlugin(
+      {
+        extensions: ['ts', 'tsx'],
+      }
+    ),
+    new webpack.DefinePlugin(
+      {
+        'process.env': env.parsed,
+      }
+    ),
+  ],
+};
+
+export default config;
